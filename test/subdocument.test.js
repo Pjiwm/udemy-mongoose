640a84b4b1bed1f87bd1ba465748c36774404be0
Pjiwm <Pim.Munne@gmail.com>
2021-11-15 17:03:04 +0100

    adding document to existing user test

--- test/subdocument.test.js
+++ test/subdocument.test.js
@@ -7,13 +7,28 @@ describe('Subdocuments', () => {
         await mongoose.connection.collections.users.drop()
     })
 
-    it('can create a Subdocuments', async () => {
+    it('Can create a Subdocument', async () => {
         const joe = new user({
             name: 'Joe',
             posts: [{ title: 'PostTitle' }]
         })
         await joe.save()
         const foundUser = await user.findOne({ name: 'Joe' })
-        assert(foundUser.posts[0].title === 'PostTitles')
+        assert(foundUser.posts[0].title === 'PostTitle')
+    })
+
+    it('Can add subdocuments to an existing record', async () => {
+        const joe = new user({
+            name: 'Joe',
+            posts: []
+        })
+        await joe.save()
+        let newUser = await user.findOne({ name: 'Joe'})
+        newUser.posts.push({ title: 'New Post' })
+        await newUser.save()
+        // we retrieve the data from the database, so we're sure that the post was pushed to DB.
+        newUser = await user.findOne({ name: 'Joe' })
+        assert(newUser.posts[0].title === 'New Post')
+
     })
 })

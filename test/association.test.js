55029f2f99c1f0c1eab68815ddba7acd79a6ee15
Pjiwm <Pim.Munne@gmail.com>
2021-11-15 19:42:59 +0100

    added test with deep associations

--- test/association.test.js
+++ test/association.test.js
@@ -5,7 +5,7 @@ const blogPost = require('../src/blog.post')
 const assert = require('assert')
 
 describe('Associations', () => {
-    
+
     let joe, newBlogPost, newComment
     beforeEach(async () => {
         const { users, blogposts, comments } = mongoose.connection.collections
@@ -29,5 +29,24 @@ describe('Associations', () => {
     it('saves a relation between a user and a blog post', async () => {
         const newUser = await user.findOne({ name: 'Joe' }).populate('blogPosts')
         assert(newUser.blogPosts[0].title === 'JS is Great')
+    })
+
+    it('saves a full relation tree', async () => {
+        const newUser = await user.findOne({ name: 'Joe' }).populate({
+            path: 'blogPosts',
+            populate: {
+                path: 'comments',
+                model: 'comment',
+                populate: {
+                    path: 'user',
+                    model: 'user'
+                }
+            }
+        })
+
+        assert(newUser.name == 'Joe')
+        assert(newUser.blogPosts[0].title == 'JS is Great')
+        assert(newUser.blogPosts[0].comments[0].content == 'Congrats on great post')
+        assert(newUser.blogPosts[0].comments[0].user.name == 'Joe')
     })
 })

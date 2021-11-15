14b906015de6251370ff09e43fe2c12606ea88ff
Pjiwm <Pim.Munne@gmail.com>
2021-11-15 17:10:14 +0100

    remove subdocument test

--- test/subdocument.test.js
+++ test/subdocument.test.js
@@ -23,12 +23,27 @@ describe('Subdocuments', () => {
             posts: []
         })
         await joe.save()
-        let newUser = await user.findOne({ name: 'Joe'})
+        let newUser = await user.findOne({ name: 'Joe' })
         newUser.posts.push({ title: 'New Post' })
         await newUser.save()
         // we retrieve the data from the database, so we're sure that the post was pushed to DB.
         newUser = await user.findOne({ name: 'Joe' })
         assert(newUser.posts[0].title === 'New Post')
+    })
 
+    it('can remove an existing subdocument', async () => {
+        let joe = new user({
+            name: 'Joe',
+            posts: [{ title: 'New title' }]
+        })
+        await joe.save()
+        // we retrieve user from db again (similar to last test)
+        joe = await user.findOne({ name: 'Joe' })
+        const post = joe.posts[0]
+        post.remove()
+        await joe.save()
+        // we retrieve again...
+        joe = await user.findOne({ name: 'Joe' })
+        assert(joe.posts.length === 1)
     })
 })
